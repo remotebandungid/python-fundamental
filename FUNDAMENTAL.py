68512f778f9077e7be66b00a25e250b5b60181b7
prayudha octavio <prayudhaocto@gmail.com>
2022-12-07 23:32:32 +0700

    FUNDAMENTAL DALAM PYTHON

--- FUNDAMENTAL.py
+++ FUNDAMENTAL.py
@@ -4,6 +4,10 @@ FUNDAMENTAL PYTHON
 
 # Sekuensial adalah kode yang dikerjakan secara urutan
 
+print("Budi membeli susu")
+print("susunya gaada")
+print("budi langsung balik")
+
 # percabangan
 
 jumlah_botol_susu = 173
@@ -24,3 +28,30 @@ print("Budi pulang kerumah")
 print("Menyampaikan hasilnya kepada ibu")
 
 # perulangan
+
+## perulangan dengan for
+
+jumlah_bukuu = 10
+print("Ibu berkata, 'Baca semua bukumu' ")
+
+jumlah_buku_yang_dibaca = 0
+
+for jumlah_buku_yang_dibaca in range(1, jumlah_bukuu+1):
+    print(f"Buku ke {jumlah_buku_yang_dibaca} sudah dibaca")
+
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku_yang_dibaca}")
+
+### perulangan dengan while
+
+jumlah_bukU_yang_dibaca_dan_dipahami = 10
+print("Ibu berkata, 'Baca semua bukumu' ")
+
+jumlah_buku_yang_dibacaa = 0
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku_yang_dibaca}")
+
+while jumlah_buku_yang_dibacaa < jumlah_bukU_yang_dibaca_dan_dipahami:
+    jumlah_buku_yang_dibacaa += 1
+    print("Baca 1 buku yang belum dibaca")
+
+
+
